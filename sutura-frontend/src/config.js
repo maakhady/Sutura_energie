194f325db5a2a72a4013c68bb210763ed599612e
maakhady <makhadypro@gmail.com>
2025-04-23 12:16:04 +0000

    CHANGEMENT URL

--- sutura-frontend/src/config.js
+++ sutura-frontend/src/config.js
@@ -8,7 +8,8 @@ const config = {
 
   },
   production: {
-    apiBaseURL: 'https://sutura-energie.onrender.com',
+    // apiBaseURL: 'https://sutura-energie.onrender.com',
+    apiBaseURL: 'https://sutura-energie-ma-khadys-projects.vercel.app',
     cameraURL: 'http://192.168.1.147:7000/video_feed', // URL locale
 
   }
