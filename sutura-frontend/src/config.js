480aaf91d95f18c477a292b7a8201c70c5fc0504
maakhady <makhadypro@gmail.com>
2025-04-24 10:21:37 +0000

    test camera

--- sutura-frontend/src/config.js
+++ sutura-frontend/src/config.js
@@ -4,12 +4,12 @@ const ENV = import.meta.env.MODE || 'development';
 const config = {
   development: {
     apiBaseURL: 'http://localhost:2500',
-    cameraURL: 'http://192.168.1.147:7000/video_feed', // URL locale
+    cameraURL: 'http://192.168.1.7:7000/video_feed', // URL locale
 
   },
   production: {
     apiBaseURL: 'https://sutura-energie.onrender.com',
-    cameraURL: 'http://192.168.1.147:7000/video_feed', // URL locale
+    cameraURL: 'http://192.168.1.7:7000/video_feed', // URL locale
 
   }
 };
